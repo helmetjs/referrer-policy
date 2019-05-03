5920afe2cfad3a81b52ac03853ad1c0173fba524
Evan Hahn <me@evanhahn.com>
2019-05-03 10:21:52 -0500

    Move some constants into getHeaderValueFromOptions so they can be garbage collected

--- index.ts
+++ index.ts
@@ -1,30 +1,23 @@
 import { IncomingMessage, ServerResponse } from 'http'
 
-const DEFAULT_POLICY = 'no-referrer'
-const ALLOWED_POLICIES: string[] = [
-  'no-referrer',
-  'no-referrer-when-downgrade',
-  'same-origin',
-  'origin',
-  'strict-origin',
-  'origin-when-cross-origin',
-  'strict-origin-when-cross-origin',
-  'unsafe-url',
-  ''
-]
-const ALLOWED_POLICIES_ERROR_LIST = ALLOWED_POLICIES.map(function (policy) {
-  if (policy.length) {
-    return '"' + policy + '"'
-  } else {
-    return 'and the empty string'
-  }
-}).join(', ')
-
 interface ReferrerPolicyOptions {
   policy?: string | string[];
 }
 
-export = function referrerPolicy (options?: ReferrerPolicyOptions) {
+function getHeaderValueFromOptions(options?: ReferrerPolicyOptions): string {
+  const DEFAULT_POLICY = 'no-referrer'
+  const ALLOWED_POLICIES: string[] = [
+    'no-referrer',
+    'no-referrer-when-downgrade',
+    'same-origin',
+    'origin',
+    'strict-origin',
+    'origin-when-cross-origin',
+    'strict-origin-when-cross-origin',
+    'unsafe-url',
+    ''
+  ]
+
   options = options || {}
 
   let policyOption: unknown
@@ -39,7 +32,14 @@ export = function referrerPolicy (options?: ReferrerPolicyOptions) {
   const policiesSeen: Set<string> = new Set()
   policies.forEach((policy) => {
     if ((typeof policy !== 'string') || (ALLOWED_POLICIES.indexOf(policy) === -1)) {
-      throw new Error('"' + policy + '" is not a valid policy. Allowed policies: ' + ALLOWED_POLICIES_ERROR_LIST + '.')
+      const allowedPoliciesErrorList = ALLOWED_POLICIES.map(function (policy) {
+        if (policy.length) {
+          return '"' + policy + '"'
+        } else {
+          return 'and the empty string'
+        }
+      }).join(', ')
+      throw new Error('"' + policy + '" is not a valid policy. Allowed policies: ' + allowedPoliciesErrorList + '.')
     }
 
     if (policiesSeen.has(policy)) {
@@ -48,7 +48,11 @@ export = function referrerPolicy (options?: ReferrerPolicyOptions) {
     policiesSeen.add(policy)
   })
 
-  const headerValue: string = policies.join(',');
+  return policies.join(',');
+}
+
+export = function referrerPolicy (options?: ReferrerPolicyOptions) {
+  const headerValue = getHeaderValueFromOptions(options)
 
   return function referrerPolicy (_req: IncomingMessage, res: ServerResponse, next: () => void) {
     res.setHeader('Referrer-Policy', headerValue)
